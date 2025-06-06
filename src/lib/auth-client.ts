84fae80cda1e8f38c20a0d232117fffa76191b1e
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-06 17:01:28 -0500

    feat(dashboard): implementar menú de usuario con opciones de perfil y facturación, adaptado para dispositivos móviles

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000" 
+     baseURL: "http://localhost:3000"
+    // baseURL: "http://192.168.1.9:3000"
 })
