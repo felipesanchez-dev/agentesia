3199c8e78c372974007ed64dcdc22bd777f1a184
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-11 11:32:37 -0500

    feat: agregar componente de paginación de datos en la vista de agentes

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -8,6 +8,7 @@ import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
   const [filters, setFilters] = useAgentsFilters();
@@ -22,11 +23,11 @@ export const AgentsView = () => {
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
       <DataTable data={data.items} columns={columns} />
-      {/* <DataPagination 
+      <DataPagination
       page={filters.page}
       totalPages={data.totalPages}
       onPageChange={(page) => setFilters({page})}
-      /> */}
+      />
       {data.items.length === 0 && (
         <EmptyState
           title="No hay agentes"
