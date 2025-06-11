e7d2c3ebe969ad5cbcc51ae7b648a954c57434ab
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-10 23:22:43 -0500

    feat: agregar paginación y búsqueda a la consulta de agentes, mejorar la visualización de reuniones y ajustar el manejo de estados vacíos

--- src/app/modules/agents/ui/components/columns.tsx
+++ src/app/modules/agents/ui/components/columns.tsx
@@ -35,7 +35,7 @@ export const columns: ColumnDef<AgentGetOne>[] = [
     cell: ({ row }) => (
       <Badge variant="outline" className="flex items-center gap-x-2 [&>svg]:size-4">
         <VideoIcon className="text-blue-700" />
-        {row.original.meetingCount || 0} reuniones
+        {row.original.meetingCount} {row.original.meetingCount === 1 ? "Reunión" : "Reuniones"}
       </Badge>
     ),
   },
--- src/app/modules/agents/ui/views/agents-view.tsx
+++ src/app/modules/agents/ui/views/agents-view.tsx
@@ -1,26 +1,25 @@
 "use client";
 
- import { useSuspenseQuery } from "@tanstack/react-query";
- import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
 import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 
-
 export const AgentsView = () => {
-   const trpc = useTRPC();
-   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({}));
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable columns={columns} data={data} />
-      {data.length === 0 && (
+      <DataTable data={data.items} columns={columns} />
+      {data.items.length === 0 && (
         <EmptyState
-         title="No hay agentes"
+          title="No hay agentes"
           description="No has creado ningún agente aún. Haz clic en el botón de abajo para crear uno nuevo."
-        />  
+        />
       )}
     </div>
   );
