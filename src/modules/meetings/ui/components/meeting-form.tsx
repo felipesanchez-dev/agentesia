e6b0b69094041cb8064382b1c7515a30180e7b59
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-14 12:59:42 -0500

    feat: agregar descripción del agente en el formulario de reuniones y lógica para crear un nuevo agente(view)

--- src/modules/meetings/ui/components/meeting-form.tsx
+++ src/modules/meetings/ui/components/meeting-form.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -19,6 +20,7 @@ import { toast } from "sonner";
 import { MeetingGetOne } from "../../types";
 import { meetingsInsertSchema } from "../../shemas";
 import { useState } from "react";
+import { NewAgentDialog } from "@/modules/agents/ui/components/new-agent-dialog";
 
 interface MeetingFormProps {
   onSuccess?: (id?: string) => void;
@@ -33,7 +35,7 @@ export const MeetingForm = ({
 }: MeetingFormProps) => {
   const trpc = useTRPC();
   const queryClient = useQueryClient();
-
+  const [openNewAgentDialog, setOpenNewAgentDialog] = useState(false);
   const [agentSearch, setAgentSearch] = useState("");
   const agents = useQuery(
     trpc.agents.getMany.queryOptions({
@@ -98,73 +100,90 @@ export const MeetingForm = ({
   };
 
   return (
-    <Form {...form}>
-      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          name="name"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nombre de la reunión</FormLabel>
-              <FormControl>
-                <Input {...field} placeholder="ej. Clase de Ingles" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="agentId"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Agente Asignado</FormLabel>
-              <FormControl>
-                <CommandSelect
-                  options={(agents.data?.items ?? []).map((agent) => ({
-                    id: agent.id,
-                    value: agent.id,
-                    children: (
-                      <div className="flex items-center gap-x-2">
-                        <AgentAvatar
-                          name={agent.name}
-                          size="sm"
-                          showIcon={true}
-                          showInitials={false}
-                          className="border-2 border-white shadow-sm"
-                        />
-                        <span className="text-sm font-medium">
-                          {agent.name}
-                        </span>
-                      </div>
-                    ),
-                  }))}
-                  onSelect={field.onChange}
-                  onSearch={setAgentSearch}
-                  value={field.value}
-                  placeholder="Selecciona un agente"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex justify-between gap-x-2">
-          {onCancel && (
-            <Button
-              variant="ghost"
-              disabled={isPending}
-              type="button"
-              onClick={() => onCancel()}
-            >
-              Cancelar
+    <>
+      <NewAgentDialog
+        open={openNewAgentDialog}
+        onOpenChange={setOpenNewAgentDialog}
+      />
+      <Form {...form}>
+        <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre de la reunión</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="ej. Clase de Ingles" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="agentId"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Agente Asignado</FormLabel>
+                <FormControl>
+                  <CommandSelect
+                    options={(agents.data?.items ?? []).map((agent) => ({
+                      id: agent.id,
+                      value: agent.id,
+                      children: (
+                        <div className="flex items-center gap-x-2">
+                          <AgentAvatar
+                            name={agent.name}
+                            size="sm"
+                            showIcon={true}
+                            showInitials={false}
+                            className="border-2 border-white shadow-sm"
+                          />
+                          <span className="text-sm font-medium">
+                            {agent.name}
+                          </span>
+                        </div>
+                      ),
+                    }))}
+                    onSelect={field.onChange}
+                    onSearch={setAgentSearch}
+                    value={field.value}
+                    placeholder="Selecciona un agente"
+                  />
+                </FormControl>
+                <FormDescription>
+                  ¿No tienes un agente especializado?
+                  <Button
+                    variant="link"
+                    type="button"
+                    className="text-primary hover:underline"
+                    onClick={() => setOpenNewAgentDialog(true)}
+                  >
+                    Crea un agente nuevo
+                  </Button>
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-between gap-x-2">
+            {onCancel && (
+              <Button
+                variant="ghost"
+                disabled={isPending}
+                type="button"
+                onClick={() => onCancel()}
+              >
+                Cancelar
+              </Button>
+            )}
+            <Button disabled={isPending} type="submit">
+              {isEdit ? "Actualizar Agente" : "Crear Agente"}
             </Button>
-          )}
-          <Button disabled={isPending} type="submit">
-            {isEdit ? "Actualizar Agente" : "Crear Agente"}
-          </Button>
-        </div>
-      </form>
-    </Form>
+          </div>
+        </form>
+      </Form>
+    </>
   );
 };
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -2,14 +2,18 @@
 
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
-// import { useTRPC } from "@/trpc/client";
-// import { useSuspenseQuery } from "@tanstack/react-query";
+ import { useTRPC } from "@/trpc/client";
+ import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const MeetingsView = () => {
-  // const trpc = useTRPC();
-  // const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
 
-  return <div>Todo: Data table</div>;
+  return (
+    <div>
+      {JSON.stringify(data)}
+    </div>
+  )
 };
 
 export const MeetingsViewLoading = () => {
