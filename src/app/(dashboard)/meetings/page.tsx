b07cdcf641cb94144d8a583f06114b2d504f9e23
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-14 11:10:31 -0500

    feat: agregar manejo de errores y estado de carga a MeetingsView

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,6 +1,12 @@
-import { MeetingsView } from "@/modules/meetings/ui/views/meetings-view";
+import {
+  MeetingsView,
+  MeetingsViewError,
+  MeetingsViewLoading,
+} from "@/modules/meetings/ui/views/meetings-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Page = () => {
   const queryClient = getQueryClient();
@@ -8,7 +14,11 @@ const Page = () => {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <MeetingsView />
+      <Suspense fallback={<MeetingsViewLoading />}>
+        <ErrorBoundary fallback={<MeetingsViewError />}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
     </HydrationBoundary>
   );
 };
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,14 +1,30 @@
-"use client"
+"use client";
 
-import { useTRPC } from "@/trpc/client"
-import { useQuery } from "@tanstack/react-query";
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const MeetingsView = () => {
-    const trpc = useTRPC();
-    const {data} = useQuery(trpc.meetings.getMany.queryOptions({}))
-    return (
-        <div>
-            {JSON.stringify(data)}
-        </div>
-    )
-}
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Cargando reuniones"
+      description="Estamos cargando tus reuniones. Por favor, espera un momento."
+    />
+  );
+};
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="No se pudieron cargar las reuniones"
+      description="Ocurrió un error al intentar cargar las reuniones. Verifica tu conexión a internet o contacta al soporte si el problema persiste."
+    />
+  );
+};
