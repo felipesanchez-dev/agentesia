5fe8ee00f5aed36867a1dbe02f36a98628eb27de
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-16 17:33:26 -0500

    feat: agregar diálogo de actualización de reunión en la vista de MeetingId

---
+++ src/modules/meetings/ui/components/update-meeting-dialog.tsx
@@ -0,0 +1,31 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { MeetingForm } from "./meeting-form";
+import { MeetingGetOne } from "../../types";
+
+interface UpdateMeetingDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  initialValues: MeetingGetOne;
+}
+
+export const UpdateMeetingDialog = ({
+  open,
+  onOpenChange,
+  initialValues
+}: UpdateMeetingDialogProps) => {
+
+  return (
+    <ResponsiveDialog
+      title="Editar Reunión"
+      description="Edita los detalles de la reunión."
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <MeetingForm
+        onSuccess={() => onOpenChange(false)}
+        onCancel={() => onOpenChange(false)}
+        initialValues={initialValues}
+      />
+    </ResponsiveDialog>
+  );
+};
--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -11,6 +11,8 @@ import {
 import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
 import { useRouter } from "next/navigation";
 import { useConfirm } from "@/hooks/use-confirm";
+import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { useState } from "react";
 
 interface Props {
   meetingId: string;
@@ -30,6 +32,8 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     "¿Estás seguro de que deseas eliminar esta reunión?",
   );
 
+  const [updateMeetingDialogOpen, setUpdateMeetingDialogOpen] = useState(false);
+
   const removeMeeting = useMutation(
     trpc.meetings.remove.mutationOptions({
       onSuccess: () => {
@@ -48,11 +52,16 @@ export const MeetingIdView = ({ meetingId }: Props) => {
   return (
     <>
     <RemoveConfirmation />
+    <UpdateMeetingDialog
+      open={updateMeetingDialogOpen}
+      onOpenChange={setUpdateMeetingDialogOpen}
+      initialValues={data}
+    />
       <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
         <MeetingIdViewHeader
           meetingId={meetingId}
           meetingName={data?.name}
-          onEdit={() => {}}
+          onEdit={() => setUpdateMeetingDialogOpen(true)}
           onRemove={handleRemoveMeeting}
         />
         {JSON.stringify(data, null, 2)}
