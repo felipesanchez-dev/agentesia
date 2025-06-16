42de318ab3535d202679fe9cc58f9439e76c6027
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-16 17:00:32 -0500

    feat: agregar consulta de reunión en la vista de MeetingId

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 interface Props {
-    meetingId: string;
-};
+  meetingId: string;
+}
 
 export const MeetingIdView = ({ meetingId }: Props) => {
-    return (
-        <>
-        <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <h1>Meeting ID: {meetingId}</h1>
-        </div>
-        </>
-    )
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
+  );
+  return (
+    <>
+      <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+        <h1>Meeting ID: {meetingId}</h1>
+        {JSON.stringify(data, null, 2)}
+      </div>
+    </>
+  );
 };
 
 export const MeetingIdViewLoading = () => {
